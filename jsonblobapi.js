1545a2d614f9e53b242482dd099f09915587bd62
rsa17826 <rsa17826@email.vccs.edu>
2025-03-23 10:13:40 -0400

    fixed trying to save 2 things at the same time only saving 1

--- jsonblobapi.js
+++ jsonblobapi.js
@@ -1,11 +1,37 @@
 class jsonblobapi {
   constructor(id) {
+    this.saving = []
     this.url = `https://jsonblob.com/api/jsonBlob/${id}`
   }
   async load() {
-    return (await globalrequest(this.url)).text
+    if (this.saving.length) await this.__notSaving()
+    this.saving.push(1)
+    var x = (await globalrequest(this.url)).text
+    this.saving.pop()
+    return x
+  }
+  async __notSaving() {
+    if (!this.saving.length) {
+      // log(this.saving)
+      return
+    }
+    return new Promise((resolve, reject) => {
+      var int = setInterval(
+        (() => {
+          // debugger
+          // log(this.saving)
+          if (!this.saving.length) {
+            clearInterval(int)
+            // setTimeout(resolve, 1000)
+            resolve()
+          }
+        }).bind(this)
+      )
+    })
   }
   async save(data) {
+    if (this.saving.length) await this.__notSaving()
+    this.saving.push(1)
     log(
       await globalrequest({
         url: this.url,
@@ -14,9 +40,10 @@ class jsonblobapi {
           "Content-Type": "application/json",
           Accept: "application/json",
         },
-        body:data,
+        body: data,
       })
     )
+    this.saving.pop()
     return
   }
 }
